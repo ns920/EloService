9709d1ca89bd0a2b0f810b847de27bd3e3ab8039
MoeAsuka <victim920@gmail.com>
2021-04-10 07:06:51 +0000

    base

--- EloService/Controllers/EloController.cs
+++ EloService/Controllers/EloController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,31 @@ namespace EloService.Controllers
                 {
                     elo1 = elo1,
                     elo2 = elo2,
+                    message = "Success"
+                };
+                return Json(context);
+            }
+            catch (System.Exception ex)
+            {
+                var context = new
+                {
+                    message = "Error",
+                };
+                return Json(context);
+            }
+        }
+
+        [Route("api/Elo/Calculate2")]
+        [HttpPost]
+        public IActionResult Calculate2(string elo1,string elo2,string matchentity)
+        {
+            try
+            {
+                var match = JsonConvert.DeserializeObject<MatchEntity>(matchentity);
+                var context = new
+                {
+                    elo1 = match.users[0].id,
+                    elo2 = match.users[1].id,
                     message = "Success"
                 };
                 return Json(context);
